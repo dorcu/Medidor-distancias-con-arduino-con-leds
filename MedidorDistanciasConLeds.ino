a313fcf73ce6b4b999f7668b042b7ebdffe9bd43
David Ortega <david.orcu@gmail.com>
2014-04-09 10:10:15 +0200

    Update MedidorDistanciasConLeds.ino

--- MedidorDistanciasConLeds.ino
+++ MedidorDistanciasConLeds.ino
@@ -3,12 +3,13 @@
 // 6 de Abril de 2014
 // hola@dorcu.com
 
+// Definimos los pines que vamos a usar
 #define trigPin 7
 #define echoPin 8
 #define led 11
 #define led2 10
 
-int dis = 10;
+int dis = 10; // Establecemos la distancia en cm para la comprobacion
 
 // Librerias necesarias para el funcionamiento
 #include "Ultrasonic.h"
@@ -18,10 +19,10 @@ Ultrasonic ultrasonido(trigPin,echoPin);
 
 void setup() {
   Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Establecemos el pin trig como salida
+  pinMode(echoPin, INPUT); // Establecemos el pin echo como entrada
+  pinMode(led, OUTPUT); // Establecemos el led verde como salida
+  pinMode(led2, OUTPUT); // Establecemos el rojo verde como salida
 }
 
 void loop() {
@@ -29,15 +30,15 @@ void loop() {
 
   distancia = ultrasonido.Ranging(CM);
 
-  if (distancia &lt; dis){
-    digitalWrite(led,LOW);
-    digitalWrite(led2,HIGH);
+  if (distancia < dis){
+    digitalWrite(led,LOW); // Apagamos el led verde
+    digitalWrite(led2,HIGH); // Encendemos el led rojo
     Serial.print("Led rojo - Distancia: ");
     Serial.print(ultrasonido.Ranging(CM)); 
     Serial.println(" cm");
   } else {
-    digitalWrite(led,HIGH);
-    digitalWrite(led2,LOW);
+    digitalWrite(led,HIGH); // Encendemos el led verde
+    digitalWrite(led2,LOW); // Apagamos el led rojo
     Serial.print("Led verde - Distancia: ");
     Serial.print(ultrasonido.Ranging(CM)); 
     Serial.println(" cm");
